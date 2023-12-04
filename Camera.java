00702c922b43ac2a59d5c01bd85506b471a6b3b7
KevinLin19 <150128883+KevinLin19@users.noreply.github.com>
2023-12-04 09:35:09 +0100

    Add files via upload

--- Camera.java
+++ Camera.java
@@ -12,9 +12,25 @@ public class Camera {
         this.x = x;
         this.y = y;
     }
-
     @Override
     public String toString() {
         return x +", "+ y ;
+    }
+
+    public void move(int X) {
+        this.x = this.x + X;
+    }
+
+    public void update(long time) {
+        /*
+        double vitesse_x=0;
+        double vitesse_y=0;
+        double acceleration_y = (150-getX())+1.2*vitesse_y;
+        double acceleration_x = (150-getX())+1.2*vitesse_x;
+        vitesse_y = acceleration_y*time;
+        vitesse_x = acceleration_x*time;
+        this.y = vitesse_y*time;
+        this.x = vitesse_x*time;
+         */
     }
 }
